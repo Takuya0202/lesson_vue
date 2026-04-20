d7290ab9da1dc518a0f7ef51ecd6c66ca4a30593
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-04-20 19:39:31 +0900

    lesson2 complete

--- lesson_vue/src/App.vue
+++ lesson_vue/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import Lesson1 from './components/lesson_1/lesson1.vue';
+import Lesson1 from './components/lesson/lesson1.vue';
+import Lesson2 from './components/lesson/lesson2.vue';
 </script>
 
 <template>
   <!-- <HelloWorld /> -->
    <Lesson1 />
+   <Lesson2 />
 </template>
---
+++ lesson_vue/src/components/lesson/lesson2.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+// useState => ref
+// Ref<number> => number型のrefを作成。
+const count = ref(0)
+
+// valueでアクセス。
+function increment() {
+    count.value += 1
+}
+
+function decrement() {
+    count.value -= 1
+}
+
+// 配列、オブジェクトに対してはreactiveを使用する。
+const user = reactive({
+    name : 'John',
+    age : 20,
+    email : 'john@example.com',
+})
+
+function updateUser() {
+    user.name = 'Jane'
+    user.age = 21
+    user.email = 'jane@example.com'
+}
+
+// 分割代入するとリアクティブにならない。
+const { name } = user
+</script>
+
+<template>
+    <!-- template内では、countでアクセス。 -->
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+
+    <p>User: {{ user.name }} - {{ user.age }} - {{ user.email }}</p>
+    <button @click="updateUser">Update User</button>
+</template>
