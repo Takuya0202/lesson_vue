d6fadb1ed1c801d5abae2692b4fb509a7f081ed9
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-04-20 19:15:19 +0900

    lesson1 complete

--- lesson_vue/src/App.vue
+++ lesson_vue/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Lesson1 from './components/lesson_1/lesson1.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+   <Lesson1 />
 </template>
--- lesson_vue/src/components/lesson_1/lesson1.vue
+++ lesson_vue/src/components/lesson_1/lesson1.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 const title = 'Vue 学習中 — 1-1 テンプレート構文'
+const isDisabled = true
+function handleClick() {
+    alert('alert表示')
+}
+const isShow = true
+const items = ['item1', 'item2', 'item3']
+const users = [
+    { name: 'John', age: 20 },
+    { name: 'Jane', age: 21 },
+    { name: 'Jim', age: 22 },
+]
 </script>
 
 <template>
@@ -7,4 +18,32 @@ const title = 'Vue 学習中 — 1-1 テンプレート構文'
         <!-- {{  }} で変数を表示 -->
         {{  title  }}
     </h1>
+    <!-- タグの変数埋め込みは : で行う -->
+    <button :disabled="isDisabled">
+        無効ボタン
+    </button>
+
+    <!-- @でイベントハンドラーを指定 -->
+    <!-- nuxtの場合、ssrは無効にする必要ない。"use client"的なものはいらない・ -->
+    <button @click="handleClick">
+        alert表示
+    </button>
+    
+    <!-- if文も使える。 -->
+     <p v-if="isShow">
+        表示される
+     </p>
+     <!-- endifがない代わりに、v-ifの直後にないとエラー -->
+     <p v-else>
+        表示されない
+     </p>
+
+     <ul>
+        <!-- react,nextのようにmapにはkey属性が必要 -->
+        <li v-for="item in items" :key="item">{{ item }}</li>
+     </ul>
+
+     <ul>
+        <li v-for="user in users" :key="user.name">{{ user.name }} - {{ user.age }}</li>
+     </ul>
 </template>
