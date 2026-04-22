d8691f515343f827171f5626f2291d1bef126010
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-04-23 00:17:23 +0900

    fix tsconfig error

--- lesson_vue/src/components/lesson/lesson4.vue
+++ lesson_vue/src/components/lesson/lesson4.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
 
 // propsを定義 import不要
-const props = defineProps<{
-    title : string
-    description ?: string
-}>
+const props = defineProps({
+    title : string,
+    description : string
+})
 
 </script>
 
 <template>
-    <h1>{{ title }}</h1>
-    <p>{{ description }}</p>
+    <h1>{{ props.title }}</h1>
+    <p>{{ props.description }}</p>
 </template>
