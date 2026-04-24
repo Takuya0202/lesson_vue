48dd2f4900f7ac61671e6216919154465e4a8d5c
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-04-24 21:19:18 +0900

    tmp

---
+++ lesson_vue/src/components/lesson/lesson7.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { InjectionKey, Ref } from 'vue'
+
+export type Theme = 'light' | 'dark'
+
+export type ThemeContext = {
+  theme: Ref<Theme>
+  toggleTheme: () => void
+}
+
+export const themeKey: InjectionKey<ThemeContext> = Symbol('theme')
+</script>
